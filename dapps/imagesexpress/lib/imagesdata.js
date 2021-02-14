71e7e6ada49882a7b8c0f57b6d29f89654805b70
ajlopez <webmaster@ajlopez.com>
2021-02-14 18:54:29 -0300

    New images in Express Dapp

--- dapps/imagesexpress/lib/imagesdata.js
+++ dapps/imagesexpress/lib/imagesdata.js
@@ -14,6 +14,13 @@ module.exports = {
         width: 1492,
         height: 952
     },
+    "0x80ac361fb84041eabd7b5ac55377bc9556ea2cfb": {
+        title: 'Landscape',
+        description: 'Watercolor painting practice',
+        name: "0x80ac361fb84041eabd7b5ac55377bc9556ea2cfb.jpg",
+        width: 746,
+        height: 476
+    },
     "0x1eb0c9073fd01476a85501e2f7dca95d6571be9c": {
         title: 'Fruits',
         description: 'Watercolor painting practice',
@@ -27,5 +34,13 @@ module.exports = {
         name: "0xc2bd6a88be57447985b93e602af415e54c63dccb.jpg",
         width: 1033,
         height: 1077
+    },
+    "0x0933543a500568e995854ef1105b5154c722f035": {
+        title: 'Fruits',
+        description: 'Watercolor painting practice',
+        name: "0x0933543a500568e995854ef1105b5154c722f035.jpg",
+        width: 517,
+        height: 538
     }
 };
+
